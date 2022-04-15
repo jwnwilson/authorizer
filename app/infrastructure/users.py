ab6c2477888c4312fe3fbb732532ca9faea1e659
Noel Wilson <jwnwilson@hotmail.co.uk>
2022-04-15 17:24:00 +0100

    Fixing bug using wrong key for user id

--- app/infrastructure/users.py
+++ app/infrastructure/users.py
@@ -1,5 +1,6 @@
 import os
 from typing import Optional
+import time
 
 import jwt
 from fastapi import Depends, Request
@@ -49,27 +50,52 @@ async def get_user_manager(user_db: SQLAlchemyUserDatabase = Depends(get_user_db
 
 
 class JWTNoDBStrategy(JWTStrategy):
+    def _get_token_data(self, token) -> dict:
+        """Decode JWT token and get the payload
+        """
+        try:
+            return decode_jwt(token, self.secret, self.token_audience)
+        except jwt.PyJWTError:
+            return {}
+    
+    def _check_token_data_expired(self, data) -> Optional[dict]:
+        """Check if JWT token has expired from it's payload
+        """
+        if data['exp'] > time.time():
+            return None
+        return data
+
+    async def read_token(
+        self, token: Optional[str], user_manager: BaseUserManager[models.UC, models.UD]
+    ) -> Optional[models.UD]:
+        data = self._get_token_data(token)
+        if self._check_token_data_expired(data):
+            return None
+        return await super().read_token(token, user_manager)
+
     def read_token_no_db(self, token: str) -> Optional[UserDB]:
         if token is None:
             return None
 
-        try:
-            data = decode_jwt(token, self.secret, self.token_audience)
-            user_id = data.get("id")
-            if user_id is None:
-                return None
-        except jwt.PyJWTError:
+        data = self._get_token_data(token)
+        user_id = data.get("user_id")
+        if user_id is None:
+            return None
+        if self._check_token_data_expired(data):
             return None
 
         try:
+            data["id"] = user_id
+            data.pop("user_id")
             return UserDB(**data)
         except ValueError:
             return None
 
     async def write_token(self, user: models.UD) -> str:
         data = user.dict()
-        data["id"] = str(data["id"])
+        data["user_id"] = str(data["id"])
         data["aud"] = self.token_audience
+        data.pop("id")
         return generate_jwt(data, self.secret, self.lifetime_seconds)
 
 
