5dbb88f23687e0d508b3a9953d377742530893d3
Noel Wilson <jwnwilson88@gmail.com>
2022-10-16 21:43:57 +0100

    Updating db version

--- infra/tf/main.tf
+++ infra/tf/main.tf
@@ -268,7 +268,7 @@ module "db" {
 
   # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
   engine               = "postgres"
-  engine_version       = "11.13"
+  engine_version       = "11.16"
   family               = "postgres11" # DB parameter group
   major_engine_version = "11"         # DB option group
   instance_class       = "db.t2.micro"
